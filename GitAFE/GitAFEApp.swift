60172869038d8400028b0761f08d5948f7d0b91f
Dimitri Klopfstein <dimitri.kft@gmail.com>
2022-05-05 16:13:10 +0200

    Initial Commit

---
+++ GitAFE/GitAFEApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitAFEApp.swift
+//  GitAFE
+//
+//  Created by dimitri on 05/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct GitAFEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
